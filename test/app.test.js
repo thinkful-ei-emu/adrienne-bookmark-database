d96e76e519e5ef6ccab03fce5e9285363e3ef0c7
David Queen <dsqjr@msn.com>
2019-07-24 14:42:03 -0400

    added test for get path

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,35 @@
 const app = require('../src/app');
-describe('server response with 200 and Hello world',()=>{
+//supertest is global variable called request
+const testData = require('./bookmarks.fixture');
+
+describe('handles GET correctly',()=>{
   it('resonse with a 200 code',()=>{
     return request(app).get('/').expect(200);
   });
+  it('returns all bookmarks',()=>{
+    return request(app)
+      .get('/bookmarks')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body).is.an('array');
+        expect(res.body).is.eql(testData);
+      });
+  });
+  it('returns specific book',()=>{
+    return request(app).get('/bookmarks/1')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body).is.eql(testData[0]);
+      });
+  });
+  it('returns a 404 if given invalid id',()=>{
+    return request(app).get('/bookmarks/asdasd')
+      .expect(404)
+      .expect({error:'invalid id'});
+  });
 });
+/* describe('Handles POST request',()=>{
+  it(' returns a 400 if input is invalid',()=>{
+    return request(app).post('/bookmarks')
+  });
+}); */
---
+++ test/bookmarks.fixture.js
@@ -0,0 +1,6 @@
+function testData(){
+  return [
+    {title:'Moby Dick',url:'http://www.disney.com',description:'a man battles a whale',rating:1},
+    {title:'Aladdin',url:'www.disney.com',description:'a homeless boy finds a lamp',rating:5}];
+}
+module.exports = {testData};
