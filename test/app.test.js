36c4ea1f464b734568ebd1010c30a3ecc21ae0d7
David Queen <dsqjr@msn.com>
2019-07-24 16:20:01 -0400

    edited test

--- test/app.test.js
+++ test/app.test.js
@@ -1,24 +1,11 @@
 const app = require('../src/app');
 //supertest is global variable called request
-const testData = require('./bookmarks.fixture');
+const testData = require('./bookmarks.fixture')();
 const knex = require('knex');
 
-const db = knex({
-  client: 'pg',
-  connection: process.env.DB_URL_TEST,
-});
-
-// before('clean the table', () => db('bookmarks').truncate());
-
-// afterEach('cleanup', () => db('bookmarks').truncate());
 
 describe('handles GET correctly',()=>{
-  // will need to be included in other describes as well
-  // beforeEach('insert bookmarks', () => {
-  //   return db
-  //     .into('bookmarks')
-  //     .insert(testData);
-  // });
+ 
 
   it('resonse with a 200 code',()=>{
     return request(app).get('/').expect(200);
@@ -29,14 +16,14 @@ describe('handles GET correctly',()=>{
       .expect(200)
       .expect((res)=>{
         expect(res.body).is.an('array');
-        expect(res.body).is.eql(testData);
       });
   });
   it('returns specific book',()=>{
     return request(app).get('/bookmarks/1')
       .expect(200)
       .expect((res)=>{
-        expect(res.body).is.eql(testData[0]);
+        expect(res.body[0]).is.an('object');
+        expect(res.body[0]).to.have.properity
       });
   });
   it('returns a 404 if given invalid id',()=>{
--- test/bookmarks.fixture.js
+++ test/bookmarks.fixture.js
@@ -3,4 +3,4 @@ function testData(){
     {title:'Moby Dick',url:'http://www.disney.com',description:'a man battles a whale',rating:1},
     {title:'Aladdin',url:'www.disney.com',description:'a homeless boy finds a lamp',rating:5}];
 }
-module.exports = {testData};
+module.exports = testData;
